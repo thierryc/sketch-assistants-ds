5443e5e0460cbcc91ccfbe00a08685a102cf891e
Thierry Charbonnel <t@ap.cx>
2020-08-12 11:22:31 -0400

    add rules

--- assistants/colors/src/index.ts
+++ assistants/colors/src/index.ts
@@ -9,13 +9,33 @@ const tokens: RuleDefinition = {
   description: 'Reports a tokens message',
 }
 
+const colorBlind: RuleDefinition = {
+  rule: async (context) => {
+    context.utils.report('Hello world')
+  },
+  name: '@ap.cx/sketch-assistant-colors/colorBlind',
+  title: 'Colors Blindless',
+  description: 'Reports a colorBlind message',
+}
+
+const similarColors: RuleDefinition = {
+  rule: async (context) => {
+    context.utils.report('Hello world')
+  },
+  name: '@ap.cx/sketch-assistant-colors/similarColors',
+  title: 'Similar Colors',
+  description: 'Reports a Similar Colors very close or too close',
+}
+
 const assistant: AssistantPackage = async () => {
   return {
     name: '@ap.cx/sketch-assistant-colors',
-    rules: [tokens],
+    rules: [tokens, colorBlind, similarColors],
     config: {
       rules: {
-        '@ap.cx/sketch-assistant-colors/tokens': { active: true },
+        '@ap.cx/sketch-assistant-colors/tokens': { active: true, severity: 3 },
+        '@ap.cx/sketch-assistant-colors/colorBlind': { active: true, severity: 2 },
+        '@ap.cx/sketch-assistant-colors/similarColors': { active: true, severity: 1 },
       },
     },
   }
